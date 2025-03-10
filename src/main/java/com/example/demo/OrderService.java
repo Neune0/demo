f16131aa696e273d9c990ef60e4a1d7181e95506
Raimon <60296186+Neune0@users.noreply.github.com>
2025-03-10 12:04:03 +0100

    order service

--- src/main/java/com/example/demo/OrderService.java
+++ src/main/java/com/example/demo/OrderService.java
@@ -1,5 +1,18 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
+
+    private final DiscountService discountService;
+
+    public OrderService(DiscountService discountService) {
+        this.discountService = discountService;
+    }
+
+    public double calculateFinalPrice(double amount){
+        return discountService.applyDiscount(amount);
+    }
     
 }
